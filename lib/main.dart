f33f9b7878225b8300bd211847caea49237ce193
muazzam ali <alimuazzam2000@gmail.com>
2021-05-10 16:50:15 -0700

    First flutter work

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:(
+        Scaffold(body: Text("MUAZZAM ALI""alimuazzam2000@gmail.com"))
+        
+      ),
+    );
+  }
+}
